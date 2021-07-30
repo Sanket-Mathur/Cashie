754a5c2659f2cdd3187e324ccc3d3ca41d756578
Sanket-Mathur <rajeev.sanket@gmail.com>
2021-07-31 02:23:00 +0530

    Added Transacion and Cart structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,66 @@
 import './App.css';
+import React, { useState, useEffect, createContext } from 'react';
+import axios from 'axios';
 import Dashboard from './Screens/Dashboard';
 import Login from './Screens/Login';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
+export const CartContext = createContext();
+export const SettingsContext = createContext();
+
 function App() {
+    const [cartItems, setCartItems] = useState([]);
+    const [allProducts, setAllProducts] = useState(null);
+    const [storeSettings, setStoreSettings] = useState(null);
+
+    const handleQtyChange = (id, type) => {
+        let index = cartItems.findIndex((item) => item._id === id);
+        if (index !== -1) {
+            let newCartItems = [...cartItems];
+            if (newCartItems[index]["qty"] === 1 && type === "decrement") return;
+            type === "increment" ? newCartItems[index]["qty"]++ : newCartItems[index]["qty"]--;
+            setCartItems(newCartItems);
+        }
+    };
+    const handleCartDelete = (id) => {
+        let index = cartItems.findIndex((item) => item._id === id);
+        let newCartItems = [...cartItems];
+        newCartItems.splice(index, 1);
+        setCartItems(newCartItems);
+    };
+    const handleSelection = (id) => {
+        console.log(allProducts)
+        let index = cartItems.findIndex((item) => item._id === id);
+        if (index === -1) {
+            let product = allProducts.find((p) => p._id === id);
+            product.qty = 1;
+            product.price = +product.price;
+            setCartItems([...cartItems, product]);
+        } else {
+            let newCartItems = [...cartItems];
+            newCartItems.splice(index, 1);
+            setCartItems(newCartItems);
+        }
+    };
+
+    useEffect(() => {
+        axios(`${process.env.REACT_APP_BACKEND_API_URL}product?limit=100000`).then((result) =>
+            setAllProducts(result.data.data.products),
+        );
+        axios(`${process.env.REACT_APP_BACKEND_API_URL}setting`).then((result) => setStoreSettings(result.data.data));
+    }, []);
+
     return (
         <div className="App">
-            <Switch>
-                <Route path="/admin" component={Dashboard} />
-                <Route path="/login" component={Login} />
-                <Redirect from="/" to="login" />
-            </Switch>
+            <CartContext.Provider value = {{ cartItems, handleCartDelete, handleQtyChange, handleSelection }}>
+                <SettingsContext.Provider value={storeSettings}>
+                    <Switch>
+                        <Route path="/admin" component={Dashboard} />
+                        <Route path="/login" component={Login} />
+                        <Redirect from="/" to="login" />
+                    </Switch>
+                </SettingsContext.Provider>
+            </CartContext.Provider>
         </div>
     );
 }
--- frontend/src/Components/Transaction.jsx
+++ frontend/src/Components/Transaction.jsx
@@ -1,11 +1,129 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment, useContext } from 'react';
+import axios from 'axios';
+import queryString from 'query-string';
+import Cart from './Cart';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Card from '@material-ui/core/Card';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import Box from '@material-ui/core/Box';
+
+import { CartContext } from "../App";
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div role="tabpanel" hidden={value !== index} id={`scrollable-auto-tabpanel-${index}`} aria-labelledby={`scrollable-auto-tab-${index}`} {...other}>
+            { value === index && (
+                <Box p={3}>
+                    <Typography component={'span'} variant={'body2'}>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+const query = {
+    limit: 100,
+    page: 0
+};
 
 function Transaction() {
+    const cartInfo = useContext(CartContext);
+
+    const [value, setValue] = useState(0);
+    const [categories, setCategories] = useState(null);
+    const [allProducts, setAllProducts] = useState(null);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    useEffect(() => {
+        async function getAllCategoriesAndProducts() {
+            let result = await axios(
+                `${
+                    process.env.REACT_APP_BACKEND_API_URL
+                }product/transaction?${queryString.stringify(query)}`,
+            );
+    
+            setCategories(result.data.data.categories);
+            setAllProducts(result.data.data.all);
+        }
+        getAllCategoriesAndProducts();
+    }, []);
+
     return (
-        <div>
-            <h1>Transaction</h1>
-        </div>
+        <Fragment>
+            <div className="dashboard-container">
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={8}>
+                        <Paper>
+                            <Tabs value={value} onChange={handleChange} indicatorColor="primary" textColor="primary" variant="scrollable" scrollButtons="auto" aria-label="scrollable auto tabs example">
+                                <Tab label="All" 
+                                />
+                                {categories && categories.map((category, index) => (
+                                        <Tab label={category.name} key={category._id}
+                                        />
+                                    ))}
+                            </Tabs>
+                            <TabPanel value={value} index={0}>
+                                <div className="tabs-wrapper d-flex text-center" style={{ justifyContent: "center", width: "100%" }}>
+                                    {allProducts && allProducts.map((product) => (
+                                        <Card className="transaction-product-card" onClick={() => cartInfo.handleSelection(product._id)} key={product._id}>
+                                            <CardActionArea>
+                                                <CardMedia component="img" alt={product.description} height="100" image={product.image} title={product.name} />
+                                                <CardContent>
+                                                    <Typography gutterBottom variant="p" component="h6">
+                                                        {product.name}
+                                                    </Typography>
+                                                </CardContent>
+                                                <Typography gutterBottom variant="p" component="h6" className="pink">
+                                                    $ &nbsp;{product.price}
+                                                </Typography>
+                                            </CardActionArea>
+                                        </Card>
+                                    ))}
+                                </div>
+                            </TabPanel>
+                            {categories && categories.map((category, index) => (
+                                    <TabPanel value={value} index={index + 1} key={category._id}>
+                                        <div className="tabs-wrapper d-flex text-center" style={{ justifyContent: "center", width: "100%" }}>
+                                            {category.items.map((product) => (
+                                                <Card className="transaction-product-card" key={product._id} onClick={() => cartInfo.handleSelection(product._id)}>
+                                                    <CardActionArea>
+                                                        <CardMedia component="img" alt={product.description} height="100" image={product.image} title={product.name} />
+                                                        <CardContent>
+                                                            <Typography gutterBottom variant="p" component="h6">
+                                                                {product.name}
+                                                            </Typography>
+                                                        </CardContent>
+                                                        <Typography gutterBottom variant="p" component="h6" className="pink">
+                                                            $ &nbsp;{product.price}
+                                                        </Typography>
+                                                    </CardActionArea>
+                                                </Card>
+                                            ))}
+                                        </div>
+                                    </TabPanel>
+                                ))}
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Paper className="p-2">
+                            <Cart />
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </div>
+        </Fragment>
     )
 }
 
-export default Transaction
+export default Transaction;
